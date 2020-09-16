bb71aa30f8f294b7452d87b6d728c7e9889f0a3b
yuehailong0824 <374748859@qq.com>
2020-09-16 21:54:30 +0800

    版本：9.16

--- run.py
+++ run.py
@@ -3,23 +3,30 @@
 # @Author  :lemon_yuehaulong
 # @Email    :374748859@qq.com
 #执行文件
-import sys, io
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="gb18030")
 from read_data import read_data
 from request import http_request
-#第一步 获取到测试数据
-all_case=read_data('text_shuju_1.xlsx','Sheet1')
-# print('获取到的数据为：',all_case_1)
-#第二步 执行测试
-print('第一条用例的数据：',all_case[0])
-login_data=all_case[0]
-ip="http://120.78.128.25:8766"
-login_response=http_request(ip+login_data[4],eval(login_data[5]),token=None,method=login_data[3])
-print(login_response)
-for i in range(1,len(all_case)):
-    test_data=all_case[i]
-    expected=eval(test_data[6])
-    token = login_response['data']['token_info']['token']
-    # ip="http://120.78.128.25:8766"
-    response=http_request(ip+test_data[4],eval(test_data[5]),'Bearer '+token,method=test_data[3])
-    print("最后的结果是：",response)
+# 方法一
+# # 步骤一：获取到测试数据
+# all_case=read_data('text_shuju_1.xlsx','Sheet1')
+# #步骤二：执行测试先登陆
+# print('第一条用例的数据{}'.format(all_case[0]))
+# uri="http://120.78.128.25:8766"
+# login_data=all_case[0]
+# login_response=http_request(uri+login_data[4],eval(login_data[5]),token=None,method=login_data[3])
+# token = login_response['data']['token_info']['token']
+# #充值
+# for i in range(1,len(all_case)):
+#     test_data=all_case[i]
+#     http_request(uri+test_data[4],eval(test_data[5]),'Bearer '+token,method=test_data[3])
+# 方法二
+Token=None
+def run(file_name,sheet_name):
+    global Token   #申明函数内外的Token值相同
+    all_case=read_data(file_name,sheet_name)
+    for test_data in all_case:
+        uri = "http://120.78.128.25:8766"
+        response=http_request(uri+test_data[4],eval(test_data[5]),token=Token,method=test_data[3])
+        if 'login' in test_data[4]:
+            Token='Bearer '+response['data']['token_info']['token']
+run('text_shuju_1.xlsx','Sheet1',)
+
