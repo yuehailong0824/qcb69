d96086779c744f0e8e822e96fcac7be973a5c7be
yuehailong0824 <374748859@qq.com>
2020-09-16 21:53:27 +0800

    版本：9.16

--- request.py
+++ request.py
@@ -3,24 +3,22 @@
 # @Author  :lemon_yuehaulong
 # @Email    :374748859@qq.com
 import requests
-def http_request(url,data,token=None,method='post'):  #注册和登陆的函数
+def http_request(url,data,token=None,method='post'):
     header= {'X-Lemonban-Media-Type': 'lemonban.v2', 'Authorization':token}
-    if method=='get':
-        result=requests.get(url,json=data,headers=header)
+    if method=='post':
+        result=requests.post(url,json=data,headers=header)
     else:
-        result = requests.post(url, json=data, headers=header)
+        result = requests.get(url, json=data, headers=header)
+    print(result.json())
     return result.json()    #这一步很重要
 if __name__ == '__main__':
-
     #登陆
-
     login_url = 'http://120.78.128.25:8766/futureloan/member/login'
     login_data = {'mobile_phone': '15220597312', 'pwd': 'lemon666'}
-    print(http_request(login_url,login_data))
     response=http_request(login_url,login_data,)
     token =response['data']['token_info']['token']
     #充值
-    header_2={'X-Lemonban-Media-Type':'lemonban.v2','Authorization':'Bearer '+token}
     recharge_url='http://120.78.128.25:8766/futureloan/member/recharge'
-    recharge_data={'member_id':'206698','amount':5000}
-    print(http_request(recharge_url,recharge_data,'Bearer '+token))
+    recharge_data={'member_id':'16907','amount':100}
+    http_request(recharge_url,recharge_data,'Bearer '+token)
+
