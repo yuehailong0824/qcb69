a747b190268486be0644339484ac21e4e80e4bf9
yuehailong0824 <374748859@qq.com>
2020-10-15 14:11:41 +0800

    修改

--- run.py
+++ run.py
@@ -5,23 +5,9 @@
 #执行文件
 from read_data import read_data
 from request import http_request
-# 方法一
-# # 步骤一：获取到测试数据
-# all_case=read_data('text_shuju_1.xlsx','Sheet1')
-# #步骤二：执行测试先登陆
-# print('第一条用例的数据{}'.format(all_case[0]))
-# uri="http://120.78.128.25:8766"
-# login_data=all_case[0]
-# login_response=http_request(uri+login_data[4],eval(login_data[5]),token=None,method=login_data[3])
-# token = login_response['data']['token_info']['token']
-# #充值
-# for i in range(1,len(all_case)):
-#     test_data=all_case[i]
-#     http_request(uri+test_data[4],eval(test_data[5]),'Bearer '+token,method=test_data[3])
-# 方法二
 Token=None
 def run(file_name,sheet_name):
-    global Token   #申明函数内外的Token值相同
+    global Token   
     all_case=read_data(file_name,sheet_name)
     for test_data in all_case:
         uri = "http://120.78.128.25:8766"
